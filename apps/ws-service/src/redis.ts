7f7cadfd4adcaed939c3bc4767947d09a5e46e13
AnujAcharjee <anujacharjee37@gmail.com>
2026-03-14 19:46:27 +0000

    fiex on message parse

--- apps/ws-service/src/redis.ts
+++ apps/ws-service/src/redis.ts
@@ -1,7 +1,11 @@
 import { createRedisClient, type RedisClient, type RedisConfig, type RedisEventHandlers } from '@repo/redis';
 import { logger } from './logger.js';
 import { WebSocketServer } from 'ws';
-import type { PubSubMessage, WsMessage } from '@repo/validation';
+import {
+  chatMessagePayloadAndReceiversSchema,
+  type ChatMessagePayloadAndReceivers,
+  type WsMessage,
+} from '@repo/validation';
 import { AppWebSocket } from './types/wss.js';
 
 const config: RedisConfig = {
@@ -32,22 +36,29 @@ export const redis: RedisClient = createRedisClient(config, eventHandlers);
 export const redisSub: RedisClient = createRedisClient(config, eventHandlers);
 
 export const initConsumer = async (channel: string, wss: WebSocketServer): Promise<void> => {
+  /**
+   * ON MESSAGE
+   * - consume msg
+   * - parse msg
+   * - get all clients for the active receivers
+   * - send ws message to all
+   */
   redisSub.on('message', (chan: string, message: string) => {
     if (chan !== channel) return;
 
     logger.debug({ channel, message }, 'Received message from Redis');
 
-    let parsed: PubSubMessage;
+    let parsed: ChatMessagePayloadAndReceivers;
     try {
-      parsed = JSON.parse(message) as PubSubMessage;
+      parsed = chatMessagePayloadAndReceiversSchema.parse(JSON.parse(message));
     } catch (err) {
       logger.error({ err, message }, 'Failed to parse Redis message');
       return;
     }
 
-    const { receiver, sender, text, room, timestamp } = parsed;
+    const { sender, text, attachments, roomId, parentId, createdAt, id, receivers } = parsed;
 
-    const clients = receiver
+    const clients = receivers
       .map((id) =>
         [...wss.clients].find(
           (client): client is AppWebSocket =>
@@ -58,7 +69,10 @@ export const initConsumer = async (channel: string, wss: WebSocketServer): Promi
 
     logger.debug({ clients: clients.length }, 'Number of clients to receive the message');
 
-    const payload = { type: 'chat_message', payload: { sender, text, room, timestamp } } as WsMessage;
+    const payload = {
+      type: 'chat_message',
+      payload: { id, parentId, sender, text, attachments, roomId, createdAt },
+    } as WsMessage;
     clients.forEach((client) => client.send(JSON.stringify(payload)));
   });
 
