d97ebc11a691426bccd91fe359989f61c1e9a35e
AnujAcharjee <anujacharjee37@gmail.com>
2026-03-20 21:42:29 +0000

    delete: user-service

--- apps/user-service/src/grpc/client.ts
+++
@@ -1,6 +0,0 @@
-import { credentials } from '@grpc/grpc-js';
-import { DbClient } from '@repo/proto';
-
-const GRPC_PORT = process.env.GRPC_PORT ?? 5051;
-
-export const dbGrpcClient = new DbClient(`localhost:${GRPC_PORT}`, credentials.createInsecure());
--- apps/user-service/src/logger.ts
+++
@@ -1,8 +0,0 @@
-import { createLogger, AppLogger } from '@repo/logger';
-
-const NODE_ENV = process.env.NODE_ENV || 'development';
-
-const isDev = NODE_ENV === 'development';
-const logLevel = NODE_ENV === 'production' ? 'info' : 'debug';
-
-export const logger: AppLogger = createLogger(logLevel, isDev);
